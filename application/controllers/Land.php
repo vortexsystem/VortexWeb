edaed7473e8e36bc1d8e9bbe8584f8c768c8e8cb
Nolan Perry <nolan.perry.sc@gmail.com>
2019-03-31 17:01:42 -0400

    Update Land.php

--- application/controllers/Land.php
+++ application/controllers/Land.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Land extends CI_Controller {
 
+	
+	function __construct()
+      {
+        parent::__construct();
+	 	$this->load->helper('url');
+		$this->load->database('robust');	   
+      }
         /**
          * Index Page for this controller.
          *
@@ -24,11 +31,15 @@ class Land extends CI_Controller {
         }
         public function regions()
         {
+		$this->load->model('Region_model', 'land');
+		$current_user = $this->session->uuid;
+		$region_array = $this->land->get_regions_by_owner_uuid($current_user, 10);
                $data = array(
         'base_href' => base_url(),
         'page_title' => 'Manage Regions',
-        'message' => 'My Message'
+        'region_array' => $region_array
 );     
+		
                 $this->load->view('template_part/header', $data);
                 $this->load->view('land/manage',$data);
 		$this->load->view('template_part/footer', $data);
