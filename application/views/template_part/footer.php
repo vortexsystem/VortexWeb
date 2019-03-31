57e8a53d9c6236d8948beec0e19724d8e29ed47f
Cody Cooper <nolan.perry.sc@gmail.com>
2019-03-31 16:50:57 +0000

    Add Table
    Signed-off-by: Cody Cooper <nolan.perry.sc@gmail.com>

--- application/views/template_part/footer.php
+++ application/views/template_part/footer.php
@@ -57,7 +57,10 @@
   <script src="resources/vendor/datatables/dataTables.bootstrap4.js"></script>
 
   <!-- Custom scripts for all pages-->
+
   <script src="resources/js/sb-admin.min.js"></script>
+  <!-- Demo scripts for this page-->
+  <script src="resources/js/demo/datatables-demo.js"></script>
 </body>
 
 </html>
