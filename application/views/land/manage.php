ab3e4831876b8f57b9595b66dcfb4c45c9e741b6
Nolan Perry <nolan.perry.sc@gmail.com>
2019-03-31 13:13:29 -0400

    Update manage.php

--- application/views/land/manage.php
+++ application/views/land/manage.php
@@ -6,7 +6,7 @@
         <div class="card mb-3">
           <div class="card-header">
             <i class="fas fa-table"></i>
-            Data Table Example</div>
+            Manage Regions</div>
           <div class="card-body">
             <div class="table-responsive">
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
