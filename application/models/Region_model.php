c065d4cb2ea7727ec4933e8a497209fdf38f3bec
Nolan Perry <nolan.perry.sc@gmail.com>
2019-03-31 16:55:38 -0400

    Create Region_model.php

---
+++ application/models/Region_model.php
@@ -0,0 +1,32 @@
+<?php
+class Region_model extends CI_Model
+
+  {
+    function __construct()
+      {
+        parent::__construct();
+        $this->load->database();
+      }
+    public function get_regions_by_owner_uuid($uuid, $limit)
+      {
+        if(isset($limit)
+        {
+        $the_limit = $limit;
+        }
+        else
+        {
+        $the_limit = "100";
+        }
+        $this->db->select('*');
+        $this->db->from('regions');
+        $this->db->where('owner_uuid', $uuid);
+        $this->db->limit($the_limit);
+        $query = $this->db->get();
+        $row = $query->result_array();
+        if (isset($row))
+          {
+            return $row;
+          }
+      }
+  }
+?>
