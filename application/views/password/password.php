ac8c9bb6fedae857be36e09c57b9441307545b53
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-01 16:59:24 -0400

    Update password.php

--- application/views/password/password.php
+++ application/views/password/password.php
@@ -16,7 +16,15 @@
 					</form>
     </div>
     <div class="col">
-      2 of 2
+      <div id="pswd_info">
+    <h4>Password must meet the following requirements:</h4>
+    <ul>
+        <li id="letter" class="invalid">At least <strong>one letter</strong></li>
+        <li id="capital" class="invalid">At least <strong>one capital letter</strong></li>
+        <li id="number" class="invalid">At least <strong>one number</strong></li>
+        <li id="length" class="invalid">Be at least <strong>8 characters</strong></li>
+    </ul>
+</div>
     </div>
   </div>
 
