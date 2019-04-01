d9b7d8efd47618b7e6473846ec38f5bb20d18630
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-01 16:55:56 -0400

    Update password.php

--- application/views/password/password.php
+++ application/views/password/password.php
@@ -1,47 +1,6 @@
-	<script>
-		var allowsubmit = false;
-		$(function(){
-			//on keypress 
-			$('#confpass').keyup(function(e){
-				//get values 
-				var pass = $('#pass').val();
-				var confpass = $(this).val();
-				
-				//check the strings
-				if(pass == confpass){
-					//if both are same remove the error and allow to submit
-					$('.error').text('');
-					allowsubmit = true;
-				}else{
-					//if not matching show error and not allow to submit
-					$('.error').text('Password not matching');
-					allowsubmit = false;
-				}
-			});
-			
-			//jquery form submit
-			$('#form').submit(function(){
-			
-				var pass = $('#pass').val();
-				var confpass = $('#confpass').val();
- 
-				//just to make sure once again during submit
-				//if both are true then only allow submit
-				if(pass == confpass){
-					allowsubmit = true;
-				}
-				if(allowsubmit){
-					return true;
-				}else{
-					return false;
-				}
-			});
-		});
-	</script>
-
-
-		
-					<form action="" id="form" method="post">
+<div class="row">
+    <div class="col">
+      <form action="" id="form" method="post">
 						<div class="form-group">
 							<label for="desc">Password:</label> 
 							<input type="password" class="form-control" name="pass" id="pass" required>
@@ -55,3 +14,12 @@
 						</div>
 						<button type="submit" name="submit" class="btn btn-default">Submit</button>
 					</form>
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+
+
+		
+					
