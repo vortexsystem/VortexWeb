77b07b2f1239e2c4427e7789c0f2a378106a4280
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-03 10:38:52 -0400

    Create Group_model.php

---
+++ application/models/Group_model.php
@@ -0,0 +1,22 @@
+<?php
+class Group_model extends CI_Model
+  {
+    function __construct()
+      {
+        parent::__construct();
+        $groups = $this->load->database('groups', TRUE);
+      }
+    public function get_user_groups($uuid)
+      {
+        $groups->select('*');
+        $groups->from('os_groups_membership');
+        $groups->where('PrincipalID', $uuid);
+        $query = $groups->get();
+        $row = $query->result_array();
+        if (isset($row))
+          {
+            return $row;
+          }
+      }
+  }
+?>
