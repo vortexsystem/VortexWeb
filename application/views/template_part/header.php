98fe610cd33e32f21346a671ba96508328bb4123
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-03 18:03:43 -0400

    Update header.php

--- application/views/template_part/header.php
+++ application/views/template_part/header.php
@@ -79,8 +79,8 @@
           <i class="fas fa-user-circle fa-fw"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">Settings</a>
-          <a class="dropdown-item" href="#">Activity Log</a>
+          <a class="dropdown-item" href="/account/profile">Profile</a>
+          <a class="dropdown-item" href="/accout/log">Activity Log</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
         </div>
