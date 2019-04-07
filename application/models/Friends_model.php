2c133d2e5bada964f548c9a7dfa3bccc55b5d4c4
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-06 20:52:12 -0400

    Create Friends_model.php

---
+++ application/models/Friends_model.php
@@ -0,0 +1,23 @@
+<?php
+class Friends_model extends CI_Model
+  {
+    function __construct()
+      {
+        parent::__construct();
+      }
+    public function get_friends($uuid)
+      {
+			  $robust = $this->load->database('robust', TRUE);
+        $robust->select('*');
+        $robust->from(`friends`);
+        $robust->where(`PrincipalID`, $uuid);
+        $robust->limit('100');
+        $query = $robust->get();
+        $row = $query->result_array();
+        if (isset($row))
+          {
+            return $row;
+          }
+      }
+  }
+?>
