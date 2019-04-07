8e3cbc617ff62f392f852c56ce0fe67ba4d6fe55
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-06 21:32:19 -0400

    Update Friends_model.php

--- application/models/Friends_model.php
+++ application/models/Friends_model.php
@@ -8,11 +8,8 @@ class Friends_model extends CI_Model
     public function get_friends($uuid)
       {
 	$robust = $this->load->database('robust', TRUE);
-        $robust->select('*');
-        $robust->from('friends');
-        $robust->where('PrincipalID', $uuid);
-        $robust->limit('100');
-        $query = $robust->get();
+	$sql = "SELECT friends.Friend, friends.Flags, concat(useraccounts.FirstName, ' ', useraccounts.LastName) AS 'name' from friends, useraccounts WHERE friends.Friend = useraccounts.principalID AND friends.PrincipalID = '" . $robust->escape($uuid) . "'";
+        $query = $robust->query($sql);
         $row = $query->result_array();
         if (isset($row))
           {
