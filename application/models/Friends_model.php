2fb365769fbd6f567899941de4af560c17f7bea0
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-06 20:58:17 -0400

    Update Friends_model.php

--- application/models/Friends_model.php
+++ application/models/Friends_model.php
@@ -7,10 +7,10 @@ class Friends_model extends CI_Model
       }
     public function get_friends($uuid)
       {
-			  $robust = $this->load->database('robust', TRUE);
+	$robust = $this->load->database('robust', TRUE);
         $robust->select('*');
-        $robust->from(`friends`);
-        $robust->where(`PrincipalID`, $uuid);
+        $robust->from('friends');
+        $robust->where('PrincipalID', $uuid);
         $robust->limit('100');
         $query = $robust->get();
         $row = $query->result_array();
