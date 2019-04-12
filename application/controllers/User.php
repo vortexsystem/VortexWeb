bf93f772e7741f859622a4f01fe4a5d666d1fb96
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-12 16:35:56 -0400

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -82,5 +82,11 @@ class User extends CI_Controller
             exit;
         }
     }
+	  public function base64url_encode($data) { 
+      return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+    } 
+    public function base64url_decode($data) { 
+      return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT)); 
+    }       
 }
 ?>
