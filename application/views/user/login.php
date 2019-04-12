571f90bf717f3e7aed06b7db1bd5b5e3f49eac90
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-12 13:47:46 -0400

    Update login.php

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -52,8 +52,8 @@ echo "<div class='alert alert-danger' role='alert'>". $message . "</div>";
             <button type="submit" class="btn btn-primary">Login</button>
             </form>
             <div class="text-center">
-               <a class="d-block small mt-3" href="register.html">Register an Account</a>
-               <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+               <a class="d-block small mt-3" href="user/register">Register an Account</a>
+               <a class="d-block small" href="user/forgot">Forgot Password?</a>
             </div>
          </div>
       </div>
