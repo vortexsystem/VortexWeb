0ff8f1c4210c58fca8518acee38e1fd16eb1e26f
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-20 17:51:02 -0400

    Update Data.php

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -1,20 +1,41 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Data extends CI_Controller {
-	public function index()
-	{
-		
-	}
-  public function groups()
+class Data extends CI_Controller
 {
-		$current_user = $this->session->uuid;
-		$robust = $this->load->database('robust', TRUE);
-    $sql = "SELECT os_groups_groups.GroupID, os_groups_groups.Name FROM os_groups_membership, os_groups_groups WHERE os_groups_membership.PrincipalID = ? AND os_groups_membership.GroupID = os_groups_groups.GroupID";
-    $query = $robust->query($sql, array($current_user));
-	  
-$array = $query->result_array();
-$myJSON = json_encode($array);
-
-echo $myJSON;
- }
+    public function index()
+    {
+        
+    }
+    public function groups()
+    {
+        $current_user = $this->session->uuid;
+        $robust       = $this->load->database('robust', TRUE);
+        $sql          = "SELECT os_groups_groups.GroupID, os_groups_groups.Name FROM os_groups_membership, os_groups_groups WHERE os_groups_membership.PrincipalID = ? AND os_groups_membership.GroupID = os_groups_groups.GroupID";
+        $query        = $robust->query($sql, array(
+            $current_user
+        ));
+        
+        $array  = $query->result_array();
+        $myJSON = json_encode($array);
+        
+        echo $myJSON;
+    }
+    public function friends()
+    {
+        $current_user = $this->session->uuid;
+        $robust       = $this->load->database('robust', TRUE);
+        /*
+        $sql = "";
+        $query = $robust->query($sql, array($current_user));
+        
+        $array = $query->result_array();
+        $myJSON = json_encode($array);
+        echo $myJSON;
+        */
+        echo "Error, Currently Unavailable";
+    }
+    public function events()
+    {
+        echo "Error, Currently Unavailable";
+    }
 }
