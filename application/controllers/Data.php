af03b7dbd20d37c1b9511d1f3a549aba3147dac1
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-20 16:42:41 -0400

    Update Data.php

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -11,6 +11,7 @@ class Data extends CI_Controller {
 		$robust = $this->load->database('robust', TRUE);
     $sql = "SELECT os_groups_groups.GroupID, os_groups_groups.Name FROM os_groups_membership, os_groups_groups WHERE os_groups_membership.PrincipalID = ? AND os_groups_membership.GroupID = os_groups_groups.GroupID";
     $robust->query($sql, array($current_user));
+	  echo $sql;
     
  }
 }
