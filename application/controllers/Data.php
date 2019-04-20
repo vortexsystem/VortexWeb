0c04e3d976639cd0dbcebd0b3838bfb407b891dc
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-20 16:40:59 -0400

    Create Data.php

---
+++ application/controllers/Data.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Data extends CI_Controller {
+	public function index()
+	{
+		
+	}
+  public function groups()
+{
+		$current_user = $this->session->uuid;
+		$robust = $this->load->database('robust', TRUE);
+    $sql = "SELECT os_groups_groups.GroupID, os_groups_groups.Name FROM os_groups_membership, os_groups_groups WHERE os_groups_membership.PrincipalID = ? AND os_groups_membership.GroupID = os_groups_groups.GroupID";
+    $robust->query($sql, array($current_user));
+    
+ }
+}
