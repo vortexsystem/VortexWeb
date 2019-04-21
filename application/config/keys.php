5b653b50601da1263631d81cf83139b35a8b875c
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-20 20:32:38 -0400

    Update and rename sendgrid.php to keys.php

--- application/config/keys.php
+++ application/config/keys.php
@@ -1,4 +1,11 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Sendgrid
 $config['api_user']   = '';
 $config['api_key']    = '';
 $config['api_format'] = ''; // 'json' or 'xml'
+
+
+// Stripe
+$config['stripe_pub_key'] = '';
+$config['stripe_pri_key'] = '';
