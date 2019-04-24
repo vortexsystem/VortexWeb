c19207fdf73b9f3fa97b08fe1126f07bd3873f7d
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 11:00:08 -0400

    Delete GloebitCurrency.php

--- application/libraries/Gloebit/GloebitCurrency.php
+++
@@ -1,8 +0,0 @@
-<?php
-class GloebitCurrency extends Gloebit {
-        public function __construct($config = array())
-        {
-                parent::__construct($config);
-                // Your own constructor code
-        }
-}
