c047c93e3fbd6774835d17dd1252010eb9d00189
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 10:54:50 -0400

    Create GloebitCurrency.php

---
+++ application/libraries/Gloebit/GloebitCurrency.php
@@ -0,0 +1,8 @@
+<?php
+class GloebitCurrency extends Gloebit {
+        public function __construct($config = array())
+        {
+                parent::__construct($config);
+                // Your own constructor code
+        }
+}
