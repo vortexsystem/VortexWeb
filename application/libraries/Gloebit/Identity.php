8b7108ffdf3c1a0d105743a415ca77b96d6643d4
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 10:51:43 -0400

    Create Identity.php

---
+++ application/libraries/Gloebit/Identity.php
@@ -0,0 +1,11 @@
+<?php
+
+class GloebitIdentity extends Gloebit {
+
+        public function __construct($config = array())
+        {
+                parent::__construct($config);
+                // Your own constructor code
+        }
+
+}
