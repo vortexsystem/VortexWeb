7b9f4ec490d586041aa87c54a0943df9c49b121c
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 11:00:40 -0400

    Delete GloebitCharacters.php

--- application/libraries/Gloebit/GloebitCharacters.php
+++
@@ -1,8 +0,0 @@
-<?php
-class GloebitCharacters extends Gloebit {
-        public function __construct($config = array())
-        {
-                parent::__construct($config);
-                // Your own constructor code
-        }
-}
