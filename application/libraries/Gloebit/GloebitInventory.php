101df948b19efbe48266523b1be0f5c8148e55aa
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 11:00:17 -0400

    Delete GloebitInventory.php

--- application/libraries/Gloebit/GloebitInventory.php
+++
@@ -1,8 +0,0 @@
-<?php
-class GloebitInventory extends Gloebit {
-        public function __construct($config = array())
-        {
-                parent::__construct($config);
-                // Your own constructor code
-        }
-}
