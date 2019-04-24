6df3147a7eec643a4ab01b3aac36e8d204db8699
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 10:54:22 -0400

    Create GloebitInventory.php

---
+++ application/libraries/Gloebit/GloebitInventory.php
@@ -0,0 +1,8 @@
+<?php
+class GloebitInventory extends Gloebit {
+        public function __construct($config = array())
+        {
+                parent::__construct($config);
+                // Your own constructor code
+        }
+}
