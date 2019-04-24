ea24ea3265501890f323d8185ebdc3598316f4c4
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 10:52:35 -0400

    Create GloebitCharacters.php

---
+++ application/libraries/Gloebit/GloebitCharacters.php
@@ -0,0 +1,8 @@
+<?php
+class GloebitCharacters extends Gloebit {
+        public function __construct($config = array())
+        {
+                parent::__construct($config);
+                // Your own constructor code
+        }
+}
