2e2a564cc0fec5cbb4627552acd418dd6d536345
Nolan Perry <nolan.perry.sc@gmail.com>
2019-04-24 10:56:35 -0400

    Create GloebitAuthorization.php

---
+++ application/libraries/Gloebit/GloebitAuthorization.php
@@ -0,0 +1,8 @@
+<?php
+class GloebitAuthorization extends Gloebit {
+        public function __construct($config = array())
+        {
+                parent::__construct($config);
+                // Your own constructor code
+        }
+}
