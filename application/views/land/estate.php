f4b76ca358c8a93e8e5f3a0b23f61ea849a81962
Nolan Perry <nolan.perry.sc@gmail.com>
2019-09-16 14:31:26 -0400

    Add Estate Settings Form

--- application/views/land/estate.php
+++ application/views/land/estate.php
@@ -106,80 +106,62 @@
                 <div class="tab-pane" id="settings">
 
                     <hr>
-                    <form class="form" action="##" method="post" id="registrationForm">
-                        <div class="form-group">
+                    <form class="form-horizontal">
+<fieldset>
 
-                            <div class="col-xs-6">
-                                <label for="first_name">
-                                    <h4>First name</h4></label>
-                                <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
-                            </div>
-                        </div>
-                        <div class="form-group">
+<!-- Form Name -->
+<legend>Estate Settings</legend>
 
-                            <div class="col-xs-6">
-                                <label for="last_name">
-                                    <h4>Last name</h4></label>
-                                <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
-                            </div>
-                        </div>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="estatesettings-name">Estate Name</label>  
+  <div class="col-md-4">
+  <input id="estatesettings-name" name="estatesettings-name" type="text" placeholder="Couldn't Get Estate Name" class="form-control input-md">
+  <span class="help-block">Your State</span>  
+  </div>
+</div>
 
-                        <div class="form-group">
+<!-- Multiple Radios -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="estatesettings-voice">Estate Voice Status</label>
+  <div class="col-md-4">
+  <div class="radio">
+    <label for="estatesettings-voice-0">
+      <input type="radio" name="estatesettings-voice" id="estatesettings-voice-0" value="1" checked="checked">
+      <b>Enable</b> Voice
+    </label>
+	</div>
+  <div class="radio">
+    <label for="estatesettings-voice-1">
+      <input type="radio" name="estatesettings-voice" id="estatesettings-voice-1" value="0">
+      <b>Disable</b> Voice
+    </label>
+	</div>
+  </div>
+</div>
 
-                            <div class="col-xs-6">
-                                <label for="phone">
-                                    <h4>Phone</h4></label>
-                                <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
-                            </div>
-                        </div>
+<!-- Multiple Radios -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="estatesettings-denyminors">Allow Minors(Under 18 In Real Life) in your Estate?</label>
+  <div class="col-md-4">
+  <div class="radio">
+    <label for="estatesettings-denyminors-0">
+      <input type="radio" name="estatesettings-denyminors" id="estatesettings-denyminors-0" value="1" checked="checked">
+      <b>Allow</b> Minors
+    </label>
+	</div>
+  <div class="radio">
+    <label for="estatesettings-denyminors-1">
+      <input type="radio" name="estatesettings-denyminors" id="estatesettings-denyminors-1" value="0">
+      <b>Block</b> Minors
+    </label>
+	</div>
+  </div>
+</div>
 
-                        <div class="form-group">
-                            <div class="col-xs-6">
-                                <label for="mobile">
-                                    <h4>Mobile</h4></label>
-                                <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
-                            </div>
-                        </div>
-                        <div class="form-group">
+</fieldset>
+</form>
 
-                            <div class="col-xs-6">
-                                <label for="email">
-                                    <h4>Email</h4></label>
-                                <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
-                            </div>
-                        </div>
-                        <div class="form-group">
-
-                            <div class="col-xs-6">
-                                <label for="email">
-                                    <h4>Location</h4></label>
-                                <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                            </div>
-                        </div>
-                        <div class="form-group">
-
-                            <div class="col-xs-6">
-                                <label for="password">
-                                    <h4>Password</h4></label>
-                                <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
-                            </div>
-                        </div>
-                        <div class="form-group">
-
-                            <div class="col-xs-6">
-                                <label for="password2">
-                                    <h4>Verify</h4></label>
-                                <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-xs-12">
-                                <br>
-                                <button class="btn btn-lg btn-success" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
-                                <button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
 
             </div>
