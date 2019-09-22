6161a7872fa574301bf11866331c728f1332119c
Nolan Perry <nolan.perry.sc@gmail.com>
2019-09-22 19:03:02 -0400

    Create contact.php

---
+++ application/views/account/contact.php
@@ -0,0 +1,29 @@
+<form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">Change Email</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Confirm Email</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+  </div>
+</form>
+<form>
+  <fieldset class="form-group">
+    <div class="form-group row">
+    <div class="col-sm-2">I want to recieve News and Communications from Neverworld Grid</div>
+    <div class="col-sm-10">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck1">
+        <label class="form-check-label" for="gridCheck1">
+          Subscribe
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Save Changes</button>
+    </div>
+  </div>
+</form>
