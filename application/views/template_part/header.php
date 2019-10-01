347467f730784dc61f4c894118b811706fcd23c0
Nolan Perry <nolan.perry.sc@gmail.com>
2019-09-30 21:22:32 -0400

    Comment out features not going to be in Alpha

--- application/views/template_part/header.php
+++ application/views/template_part/header.php
@@ -49,8 +49,8 @@ document.getElementById("gridname").innerHTML = site.gridname;
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
     </button>
-
-    <!-- Navbar Search -->
+<!--
+   
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <div class="input-group">
         <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
@@ -59,43 +59,17 @@ document.getElementById("gridname").innerHTML = site.gridname;
             <i class="fas fa-search"></i>
           </button>
         </div>
-      </div>
-    </form>
+	    </div></form>
+-->
 
-    <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
-       <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-bell fa-fw"></i>
-          <span class="badge badge-danger">9+</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-envelope fa-fw"></i>
-          <span class="badge badge-danger">7</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+
+    
      <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-user-circle fa-fw"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="/account/profile">Profile</a>
-          <a class="dropdown-item" href="/accout/log">Activity Log</a>
-          <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
         </div>
       </li>
@@ -108,7 +82,7 @@ document.getElementById("gridname").innerHTML = site.gridname;
 
     <!-- Sidebar -->
     <ul class="sidebar navbar-nav">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="<?= $base_href ?>"">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span>
@@ -133,27 +107,30 @@ document.getElementById("gridname").innerHTML = site.gridname;
         </a>
         <div class="dropdown-menu" aria-labelledby="accountDropdown">
           <a class="dropdown-item" href="account/">Account Summary</a>
+						  <!--
           <a class="dropdown-item" href="account/transactions">Transactions</a>
           <a class="dropdown-item" href="account/email">Email Settings</a>
           <a class="dropdown-item" href="account/contact">Contact Information</a>
           <a class="dropdown-item" href="account/password">Change Password</a>
           <a class="dropdown-item" href="account/partners">Partners</a>
+							  -->
         </div>
 
       </li>
-							  
+<!--
 <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="storeDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Store</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="storeDropdown">
+<a class="nav-link dropdown-toggle" href="#" id="storeDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<i class="fas fa-fw fa-folder"></i>
+<span>Store</span>
+</a>
+<div class="dropdown-menu" aria-labelledby="storeDropdown">
           <a class="dropdown-item" href="store/">Store</a>
           <a class="dropdown-item" href="store/subscriptions">Subscriptions</a>
           <a class="dropdown-item" href="store/buy">Buy</a>
         </div>
 
-      </li>							  
+      </li>	
+-->						
     </ul>
 
     <div id="content-wrapper">
