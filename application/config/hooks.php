235d85f46f40e616ea10f2bd6291cfd14c3b43e4
Nolan Perry <nolan.perry.sc@gmail.com>
2019-10-13 18:17:33 -0400

    Update hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -8,21 +8,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
 |
-|	https://codeigniter.com/user_guide/general/hooks.html
+|    https://codeigniter.com/user_guide/general/hooks.html
 |
 */
 
 $hook['post_controller_constructor'] = function()
 {
-	 // load the instance
-      $this->CI =& get_instance();
-     
-       if($this->CI->uri->segment(1) !== "user"){
-	              if($this->CI->uri->segment(1) !== "offline"){
-	if(empty($_SESSION['logged_in']))
-		{
-			redirect('user/login');
-		}
-       }
-       }
+    // load the instance
+    $this->CI =& get_instance();
+    
+    if ($this->CI->uri->segment(1) !== "user" or "offline" or "public") {
+        
+        
+        if (empty($_SESSION['logged_in'])) {
+            redirect('user/login');
+            
+        }
+    }
 };
